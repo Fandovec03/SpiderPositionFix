7999bf5e03bcb8ec635b85fd4f745da05822806d
Fandovec03 <Fandovec03@gmail.com>
2024-10-19 14:57:23 +0200

    Slightly loosened tollerance, reworked somestuff, make all values apply per each instance

--- SpiderPositionFix/Patches/SpiderPositionPatch.cs
+++ SpiderPositionFix/Patches/SpiderPositionPatch.cs
@@ -1,54 +1,75 @@
 using HarmonyLib;
+using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.AI;
 
 namespace SpiderPositionFix.Patches
 {
+    class spiderPositionData
+    {
+        public bool isInWallState = false;
+        public float returningFromWallState = 0f;
+        public bool startPatch = false;
+    }
     [HarmonyPatch(typeof(SandSpiderAI))]
     public class SpiderPositionPatch
     {
-        private static bool isInWallState = false;
-        private static float returningFromWallState = 0f;
-        private static bool startPatch = false;
+        static Dictionary<SandSpiderAI, spiderPositionData> spiderData = [];
 
         [HarmonyPatch("Start")]
         [HarmonyPostfix]
         static void StartPostfix(SandSpiderAI __instance)
         {
             __instance.agent.areaMask &= ~(1 << NavMesh.GetAreaFromName("Jump"));
-            startPatch = true;
+            spiderData.Add(__instance,new spiderPositionData());
+            spiderData[__instance].startPatch = true;
+        }
+
+        [HarmonyPatch("LateUpdate")]
+        [HarmonyPostfix]
+        static void UpdatePositionFix(SandSpiderAI __instance)
+        {
+            Vector3 previousPositon = __instance.meshContainer.transform.position;
+            if (!__instance.lookingForWallPosition && __instance.moveTowardsDestination && spiderData[__instance].isInWallState)
+            {
+                __instance.agent.transform.position = RoundManager.Instance.GetNavMeshPosition(__instance.meshContainer.transform.position);
+                __instance.meshContainer.transform.position = previousPositon;
+            }
         }
 
         [HarmonyPatch("LateUpdate")]
         [HarmonyPostfix]
         static void MeshContainerPositionFix(SandSpiderAI __instance)
-        {       
-            if (startPatch != true) return;
+        {
+            spiderPositionData instanceData = spiderData[__instance];
+           if (instanceData.startPatch != true) return;
 
-            if (!__instance.lookingForWallPosition && !__instance.gotWallPositionInLOS && !isInWallState)
+            if (!__instance.lookingForWallPosition && !__instance.gotWallPositionInLOS && !instanceData.isInWallState)
             {
-                if (Vector3.Distance(__instance.meshContainerPosition, __instance.transform.position) > 0.66f)
+                //InicialScript.Logger.LogDebug("Spider: wallState: " + instanceData.isInWallState);
+                if (Vector3.Distance(__instance.meshContainerPosition, __instance.transform.position) > 0.75f && !__instance.onWall)
                 {
-                    if (__instance.agent.velocity.magnitude > 0f)
+                    if (__instance.agent.velocity.normalized.magnitude > 0f && !__instance.onWall)
                     {
-                        __instance.meshContainer.rotation = __instance.gameObject.transform.rotation;
+                        __instance.meshContainer.transform.rotation = __instance.gameObject.transform.rotation;
                     }
-                    __instance.meshContainerPosition = Vector3.Slerp(__instance.meshContainerPosition, __instance.transform.position, Distance(Vector3.Distance(__instance.meshContainerPosition, __instance.transform.position), 0.5f) * Time.deltaTime);
+                    __instance.meshContainerPosition = Vector3.Lerp(__instance.meshContainerPosition, __instance.transform.position, Distance(Vector3.Distance(__instance.meshContainerPosition, __instance.transform.position), 0.5f) * Time.deltaTime);
+                    //InicialScript.Logger.LogDebug("Spider: SLERP distance: " + Vector3.Distance(__instance.meshContainerPosition, __instance.transform.position));
                 }
             }
-            if (__instance.lookingForWallPosition && __instance.gotWallPositionInLOS && !isInWallState)
+            if (__instance.lookingForWallPosition && __instance.gotWallPositionInLOS && !instanceData.isInWallState || __instance.onWall)
             {
-                isInWallState = true;
+                instanceData.isInWallState = true;
+                //InicialScript.Logger.LogDebug("Spider: wallState2: " + instanceData.isInWallState);
             }
-            if (__instance.lookingForWallPosition && __instance.gotWallPositionInLOS && isInWallState)
+            if (!__instance.lookingForWallPosition && __instance.moveTowardsDestination && instanceData.isInWallState)
             {
-                returningFromWallState += Time.deltaTime;
-
-                if (isInWallState && Vector3.Distance(__instance.meshContainerPosition, __instance.transform.position - __instance.meshContainerPosition) < 1f || returningFromWallState > 10f)
+                if (instanceData.isInWallState && Vector3.Distance(__instance.meshContainerPosition, __instance.transform.position) < 1f || instanceData.returningFromWallState > 6f)
                 {
-                    isInWallState = false;
-                    returningFromWallState = 0f;
+                    //InicialScript.Logger.LogDebug("Spider: returning from wall state. Distance: "+Vector3.Distance(__instance.meshContainerPosition, __instance.transform.position));
+                    instanceData.isInWallState = false;
+                    instanceData.returningFromWallState = 0f;
                 }
             }
         }
